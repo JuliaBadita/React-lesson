7c2ea852b02873422f999799f8b95457e96aad31
julia <juliabadita@live.fr>
2022-03-31 19:29:27 +0200

    adding the navigation component to my repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* path="/" le / = la home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* path => chemin que l'on voit dans l'url, ex : /about => quand il y a about
+    dans l'url ici on va chercher le composant (élément <About />) */}
+        <Route path="/about" element={<About />} />
+
+        {/* path='*' fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navlink } from 'react-router-dom'
+
+const navigation = () => {
+  return (
+    <div className="navigation">
+      <ul></ul>
+    </div>
+  )
+}
+
+export default navigation
