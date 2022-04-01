e0c8b4a3a852a9c474047f8a6b1ac6126c3f9e54
julia <juliabadita@live.fr>
2022-04-01 15:18:49 +0200

    adding another input to upgrade a little the home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,8 @@ import React from 'react'
 const Card = ({ country }) => {
   return (
     <li className="card">
-      {/* en les appelant comme ça, cela permet d'afficher le svg des payes en question (que l'on a défini dans la const Card) */}
+      {/* en les appelant comme ça, cela permet d'afficher le svg des payes en question (que l'on a défini dans la const Card) 
+      Ce sont toutes des données dynamiques que l'on appelle dans les {country. ....}*/}
       <img
         src={country.flags.svg}
         // avec cette manière d'écrire le alt, on lui dit de chercher le nom du pays dans countries > translations > fra > common (dans le dév tools partie components)
@@ -12,7 +13,14 @@ const Card = ({ country }) => {
         alt={'drapeau ' + country.translations.fra.common}
       />
       <div className="infos">
-        <h2>Nom du pays</h2>
+        {/* pour afficher le nom du pays (ici en hover) même principe que juste au dessus : */}
+        <h2>{country.translations.fra.common}</h2>
+
+        {/* même principe ici pour la capitale on la retrouve dans la base de données dans components > countries > hooks : State > capital*/}
+        <h4>{country.capital}</h4>
+
+        {/* ici le .toLocaleString() => séparateur de millier >>> au lieu de 3473727 cela donne 3 473 727 */}
+        <p>Pop. {country.population.toLocaleString()}</p>
       </div>
     </li>
   )
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -8,6 +8,12 @@ const Countries = () => {
   // on déclare que c'est un useState, tout comme le useEffect il faut le déclarer en faut dans l'import
   const [data, setData] = useState([])
 
+  // on définit la variable rangeValue > pour changer la valeur de ce chiffre qu'on va couper (.slice) c'est avec setRangeValue c'est un useState
+  // pour la définir il est bien d'utiliser des multiples de 12 (c'est mieux visuelement)
+  const [rangeValue, setRangeValue] = useState(36)
+
+  const radios = ['Africa', 'America', 'Asia', 'Europe', 'Oceania']
+
   // Le useEffect se joue lorsque le composant est monté (quand on l'appelle en gros)
   useEffect(() => {
     //   On lui dit va me chercher ce qu'il y a dans le lien et ensuite affiche le moi
@@ -22,23 +28,41 @@ const Countries = () => {
 
   return (
     <div className="countries">
-      <h1>Countries</h1>
+      <ul className="radio-container">
+        {/* il est préférable d'écrire defaultValue plutôt que value, car le premier permet de modifier le nombre de pays contrairement au deuxième)
+          defaultValue = la valeur par défaut >> il démarre à rangeValue ensuite avec un évènement (ici onChange) on lui demande de modifier la valeur de rangeValue*/}
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+
+        {/* on va mapper les radios  */}
+        {radios.map((continent) => (
+          <li>
+            {/* avec le name="continentRadio" */}
+            <input type="radio" id={continent} name="continentRadio" />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
       <ul>
-        {
-          //   on lui dit comment appeler chaque élément individuellement :
-          //  appelle un country, => ce qu'il va faire de chacun de ces élements/country = créer un li
+        {/*  on lui dit comment appeler chaque élément individuellement :
+           appelle un country, => ce qu'il va faire de chacun de ces élements/country = créer un li 
+           le .slice(0,rangeValue) = valeur dynamique du nombre de pays qu'on veut afficher*/}
 
-          data.map((country, index) => (
-            // (((((dans le li : on regarde la console dans components pour voir comment les lister : {countries.translations.fra.common} pour les appeler :
-            // dans components on va dans countries ensuite dans la cat translations pour la langue, ensuite la cat fra = on la choisit pour les pays en fr et la cat common = là où sont écrit le nom de pays
-            // <li key={index}>{country.translations.fra.common}</li>))))))
+        {data.slice(0, rangeValue).map((country, index) => (
+          // (((((dans le li : on regarde la console dans components pour voir comment les lister : {countries.translations.fra.common} pour les appeler :
+          // dans components on va dans countries ensuite dans la cat translations pour la langue, ensuite la cat fra = on la choisit pour les pays en fr et la cat common = là où sont écrit le nom de pays
+          // <li key={index}>{country.translations.fra.common}</li>))))))
 
-            //  On doit leur créer une clé unique à chacun donc avec index cela va compter 0, 1, 2 jusqu'à 250 donc chaque pays aura une clé unique qui sera son numéro
-            // maintenant avec la clé index cela apparait quand on va dans components > countries
-            // + on lui dit de recupérer country
-            <Card key={index} country={country} />
-          ))
-        }
+          //  On doit leur créer une clé unique à chacun donc avec index cela va compter 0, 1, 2 jusqu'à 250 donc chaque pays aura une clé unique qui sera son numéro
+          // maintenant avec la clé index cela apparait quand on va dans components > countries
+          // + on lui dit de recupérer country
+          <Card key={index} country={country} />
+        ))}
       </ul>
     </div>
   )
